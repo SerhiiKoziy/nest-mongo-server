b5f54f4e039d2bc5c238819d8ad29c66daa5e0c8
serhiikoziy <serhiikoziy@gmail.com>
2023-04-17 17:08:20 +0200

    add base version read

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { ClientSession, Schema as MongooseSchema } from 'mongoose';
+import { UserRepository } from '../../repositories/user.repository';
+import { CreateUserDto } from './dto/createUser.dto';
+import { User } from "../../entities/user.entity";
+
+@Injectable()
+export class UserService {
+    constructor(private readonly userRepository: UserRepository) {}
+
+    async createUser(createUserDto: CreateUserDto, session: ClientSession) {
+        const createdUser = await this.userRepository.createUser(createUserDto, session);
+        return createdUser;
+    }
+
+    async getUserById(id: MongooseSchema.Types.ObjectId): Promise<User> {
+        return await this.userRepository.getUserById(id);
+    }
+}
