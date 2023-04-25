9506491e8535f60ab5c5c153bcc2642965786a7b
serhiikoziy <serhiikoziy@gmail.com>
2023-04-25 16:25:39 +0200

    fix pathes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
-import {ValidationPipe} from "./pipes/validation.pipe";
+import { ValidationPipe } from './pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
